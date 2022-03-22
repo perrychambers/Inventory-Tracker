eae537114c18e4d5705abaa4cbffd5e9fb77b13f
perrychambers <31519869+perrychambers@users.noreply.github.com>
2022-03-21 22:02:51 -0400

    Added mongo to the code, having trouble importing client from server to routes, doing so created a circular dependency. Will need to refactor and figure out another way.

--- back-end/routes/api/index.js
+++ back-end/routes/api/index.js
@@ -1,18 +1,38 @@
+const client = require('../../src/server')
+
 myStats = '{"name":"Perry", "age":24, "maidens":null}';
 
 myStatsP = JSON.parse(myStats);
 
 stat = myStatsP.name;
 
-console.log(stat);
-
-/**
- * Middleware Endpoint
- * This gets triggered on every GET endpoint that is called
- * We can log who gets data from the server.
- */
-
 module.exports = function(app) {
+    /**
+     * Middleware Endpoint
+     * This gets triggered on every GET endpoint that is called
+     * We can log who gets data from the server.
+     */
+    app.use(function(req, res, next) {
+        console.log(req.url, req.method, req.ip)
+        next();
+    })
+
+    /**
+     * Test endpoint for interacting with mongodb
+     * We have some example data in there already that we can grab
+     * This will grab a list of the database names 
+     * Having trouble, adding an import to server creates a circular dependency...
+     */
+    app.get('/databases', async(req, res) => {
+        var databaseList = await client.db().admin().listDatabases()
+
+        console.log('Databases:')
+        databaseList.forEach(db => console.log(` - ${db.name}`))
+
+        JSON.stringify(databaseList)
+        res.json({databases: databaseList})
+    })
+
     app.get('/', (req, res) => {
         res.send('Successful response.');
     })
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 
+const { MongoClient } = require ('mongodb')
+
 const app = express();
 
+// Import routes so that the express client can see them
 require('../routes/api')(app);
+
+const uri = "mongodb+srv://admin:admin@cluster0.ivgkh.mongodb.net/test"
+
+console.log('Connecting to mongodb server...')
+const client = new MongoClient(uri)
+
+try {
+    client.connect();
+} catch (e) {
+    console.error(e)
+}
 
 // Listen on port 3000, will eventually change to work with env variables
 var server = app.listen(3000, function() {
     console.log('Listening on port ' + server.address().port);
 })
 
-//module.exports = app;
+module.exports = { client }
